41c05544e707ac62756a002640e04fec9371e291
Suvarn Pillay <2450891@students.wits.ac.za>
2024-10-20 16:18:33 +0200

    improvements

---
+++ cooler agent/v5/agent_two_v5.py
@@ -0,0 +1,119 @@
+from stable_baselines3 import DQN, HerReplayBuffer
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.vec_env import DummyVecEnv
+from replay_buffer import PrioritizedReplayBuffer
+from env import Gym2OpEnv
+import json
+import time
+
+env = Gym2OpEnv()
+gym_env = DummyVecEnv([lambda: Monitor(env)])
+
+policy_kwargs = dict(net_arch=[128, 128])  # Two layers of 128 units each
+
+# DQN model
+model = DQN(
+    "MlpPolicy",
+    gym_env,
+    learning_rate=0.001,
+    batch_size=32,
+    gamma=0.99,
+    exploration_fraction=0.1,
+    exploration_final_eps=0.01,
+    target_update_interval=500,
+    verbose=1,
+    tensorboard_log="./tb_logs/",
+    policy_kwargs=policy_kwargs,  # Use the custom architecture
+    replay_buffer_class=PrioritizedReplayBuffer
+)
+
+# Train the model
+model.learn(total_timesteps=15000)
+
+# Save and load the model
+model.save("DQN_GRID.pt")
+model.load("DQN_GRID.pt")
+
+nb_episode_test = 5
+seeds_test_env = (0, 1, 2, 3, 4, 5)    # same size as nb_episode_test
+seeds_test_agent = (3, 4, 5, 6, 7)  # same size as nb_episode_test
+ts_ep_test =  (0, 1, 2, 3, 4, 5)       # same size as nb_episode_test
+
+ep_infos = {}  # information that will be saved
+
+for ep_test_num in range(nb_episode_test):
+    init_obs, init_infos = env.reset(seed=seeds_test_env[ep_test_num])
+    model.set_random_seed(seeds_test_agent[ep_test_num])
+    done = False
+    cum_reward = 0
+    step_survived = 0
+    obs = init_obs
+    while not done:
+        act, _states = model.predict(obs, deterministic=True)
+        obs, reward, terminated, truncated, info = env.step(act)
+        step_survived += 1
+        cum_reward += float(reward)
+        done = terminated or truncated
+    ep_infos[ep_test_num] = {"time serie id": ts_ep_test[ep_test_num],
+                             "time serie folder": env._gym_env.init_env.chronics_handler.get_id(),
+                             "env seed": seeds_test_env[ep_test_num],
+                             "agent seed": seeds_test_agent[ep_test_num],
+                             "steps survived": step_survived,
+                             "total steps": int(env._gym_env.init_env.max_episode_duration()),
+                             "cum reward": cum_reward}
+    
+print(json.dumps(ep_infos, indent=4))
+
+
+
+'''
+RESULTS
+
+{
+    "0": {
+        "time serie id": 0,
+        "time serie folder": "/home/suvarn/data_grid2op/l2rpn_case14_sandbox/chronics/0039",
+        "env seed": 0,
+        "agent seed": 3,
+        "steps survived": 1060,
+        "total steps": 8064,
+        "cum reward": 369.46648044884205
+    },
+    "1": {
+        "time serie id": 1,
+        "time serie folder": "/home/suvarn/data_grid2op/l2rpn_case14_sandbox/chronics/0040",
+        "env seed": 1,
+        "agent seed": 4,
+        "steps survived": 511,
+        "total steps": 8064,
+        "cum reward": 184.90672197937965
+    },
+    "2": {
+        "time serie id": 2,
+        "time serie folder": "/home/suvarn/data_grid2op/l2rpn_case14_sandbox/chronics/0041",
+        "env seed": 2,
+        "agent seed": 5,
+        "steps survived": 4543,
+        "total steps": 8064,
+        "cum reward": 1609.5536445081234
+    },
+    "3": {
+        "time serie id": 3,
+        "time serie folder": "/home/suvarn/data_grid2op/l2rpn_case14_sandbox/chronics/0042",
+        "env seed": 3,
+        "agent seed": 6,
+        "steps survived": 805,
+        "total steps": 8064,
+        "cum reward": 291.43159997463226
+    },
+    "4": {
+        "time serie id": 4,
+        "time serie folder": "/home/suvarn/data_grid2op/l2rpn_case14_sandbox/chronics/0043",
+        "env seed": 4,
+        "agent seed": 7,
+        "steps survived": 520,
+        "total steps": 8064,
+        "cum reward": 184.07113948464394
+    }
+}
+'''
